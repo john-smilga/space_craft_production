5cc9726f4cf3b96d632aaceed05e1d5874ddd5e3
Coding Addict <johnsmilga@gmail.com>
2025-12-06 19:33:12 -0500

    fix front-end fetching bug

--- front-end/lib/axios.ts
+++ front-end/lib/axios.ts
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
+const getBaseUrl = () => {
+  if (process.env.NEXT_PUBLIC_API_URL) return process.env.NEXT_PUBLIC_API_URL;
+  return 'http://localhost:8000/api';
+};
+
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000/api',
+  baseURL: getBaseUrl(),
   withCredentials: true,
 });
 
